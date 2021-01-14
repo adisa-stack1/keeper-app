55de42098f7bb5a5e6d6bbad5f0733131df7dd2c
Abdul Ibrahim <71322125+adisa-stack1@users.noreply.github.com>
2021-01-14 23:15:12 +0100

    Add name to footer Open Footer.jsx and write name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright Abdul Ibrahim ⓒ {year}</p>
     </footer>
   );
 }
